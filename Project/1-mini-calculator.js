f5ba01be1c8d5e42b5fa91753add64ccf28dec15
Rayhanxpplg2 <rehunsinambela1234@gmail.com>
2025-10-06 07:23:49 +0700

    Rayhan Ramdhan Sinambela

---
+++ Project/1-mini-calculator.js
@@ -0,0 +1,36 @@
+//tambah
+const tambah = (a,b) => {
+    console.log(`hasil dari ${a} + ${b} adalah ${a+b} `);
+}
+tambah(4,2)
+
+//kali 
+const kali =(a,b) => {
+     console.log(`hasil dari ${a} * ${b} adalah ${a*b} `);
+}
+kali(10,2)
+
+//pengurangan 
+const kurang = (a,b) => {
+    console.log(`hasil dari ${a} - ${b} adalah ${a-b} `);
+}
+kurang(10,5)
+
+//pembagian
+const bagi = (a,b) => {
+    console.log(`hasil dari ${a} / ${b} adalah ${a/b} `);
+}
+bagi(20,2)
+
+//perpangkatan
+const pangkat = (a,b) => {
+    console.log(`hasil dari ${a} ** ${b} adalah ${a**b} `);
+}
+pangkat(3,3)
+
+//sisa bagi / modulus
+const modulus = (a,b) => {
+    console.log(`hasil dari ${a} % ${b} adalah ${a%b} `);
+}
+modulus(5,2)
+
